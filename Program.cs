c4127fd9c8396d5951ecf9c770b69620fb2d1ccd
Anuj Kumar <96485818+anujk01@users.noreply.github.com>
2022-03-22 22:53:28 +0530

    [Anuj] Add: Multiple Rides

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            RideRepository invoiceGenerator = new RideRepository(RideType.NORMAL);
+
+            double fare = invoiceGenerator.AddRide();
+            Console.WriteLine(fare);
+        }
+    }
+}
